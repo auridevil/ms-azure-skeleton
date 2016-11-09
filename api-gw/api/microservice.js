1002fa40211a00d8a0a536a36fa4b3161bcc705f
auridevil <onailerua@hotmail.com>
2016-11-09 10:10:34 +0100

    first starting version!

--- api-gw/api/microservice.js
+++ api-gw/api/microservice.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var SWAGGER_FILE = 'swagger/apis/message.yaml';
+var SWAGGER_FILE = 'swagger/apis/microservice.yaml';
 
 var path = require('path');
 var $RefParser = require('json-schema-ref-parser');
@@ -8,18 +8,17 @@ var tools = require('../lib/tools');
 
 var API_NAME = path.basename(__filename, '.js');
 
-module.exports = function(server, options, done) {
+module.exports = function (server, options, done) {
 
   var seneca = options.seneca;
   var parser = new $RefParser();
-  var requestProxyFactory = tools.requestProxyFactoryBuilder(seneca, API_NAME);
-
+  var requestHandlerFactory = tools.requestHandlerFactoryBuilder(seneca, API_NAME);
   var swaggering = tools.Swaggering(API_NAME);
 
   seneca.log.info(API_NAME, 'init');
 
   parser.dereference(SWAGGER_FILE)
-    .then(function(schema) {
+    .then(function (schema) {
       server.register({
         register: swaggering,
         options: {
@@ -28,14 +27,18 @@ module.exports = function(server, options, done) {
 
           // endpoints handlers hashmap. it shall match the Swagger endpoints
           handlers: {
-            'messages': {
-              broadcast: {
-                $post: requestProxyFactory('post', 'broadcast')
+            'ms': {
+              $get: requestHandlerFactory('get'),
+              $post: requestHandlerFactory('post'),
+              '{id}': {
+                $get: requestHandlerFactory('get'),
+                $put: requestHandlerFactory('put'),
+                $delete: requestHandlerFactory('delete')
               }
             }
           }
         }
-      }, function(err) {
+      }, function (err) {
         if (err) {
           seneca.log.error('API-GW: ' + API_NAME, 'swaggering', err);
         } else {
@@ -43,8 +46,8 @@ module.exports = function(server, options, done) {
         }
       });
       done();
-    }).catch(function(err) {
-    done(err);
-  });
-
+    }).catch(
+    function (err) {
+      done(err);
+    });
 };
